130b286ea7875b7646309a8a408507b7908889f2
Fajis PM <fajisfajar@gmail.com>
2025-06-19 15:45:18 +0300

    checking template

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  document.getElementById('response-message').textContent = "Message sent! (Not really — this is a static site.)";
-});
